8caf1f161254d4515733ff73e1005e2951f41e2e
Sean Ford <seanford@protonmail.com>
2024-01-11 14:32:49 -0800

    Add intial files with functions stubs

---
+++ src/scraper.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+class Scraper:
+    def get_webpage_html():
+        pass 
+
+    # TODO
+    def parse_data(response_html):
+        pass
+
+    # TODO
+    def write_to_csv(data):
+        pass
