8e47a13dee78e6120cbfa5d7cbc1609e1cda8b92
Sean Ford <seanford@protonmail.com>
2024-01-21 10:11:20 -0800

    Finish implementing parse_data

--- src/goodreadsranker/scraper.py
+++ src/goodreadsranker/scraper.py
@@ -11,24 +11,43 @@ def get_webpage_html():
 
 # String -> ListOfBookDicts
 def parse_data(response_html):
+    books = []
+    book_attributes = ['Title', 'Author', 'Avg rating', 'Number of Ratings', 'Year Published']
+
     #Soupify
     soup = BeautifulSoup(response_html, 'html.parser')
     #For each book entry, construct book dict
     results = soup.find_all(class_ ='elementList')
 
     for book_element in results:
+        book_dict = dict.fromkeys(book_attributes)
         book_title = book_element.find(class_ = 'bookTitle')
 
         # book_element must not be a book entry
         if (book_title == None):
             continue
+        
+        #Strip format of book from title. E.g. (Kindle Edition)
+        book_title = re.sub(r' \([\w\s]+\)', '', book_title.text)
 
-        author_name = book_element.find(class_ = 'authorName').text
+        book_dict['Title'] = book_title
+        book_dict['Author'] = book_element.find(class_ = 'authorName').text
 
         sub_text = book_element.select('span.greyText.smallText')[0].text
         avg_rating, num_ratings, year = extract_sub_text(sub_text)
+        num_ratings = num_ratings.replace(',', '')
 
-        print (avg_rating, num_ratings, year)
+        # Not a big deal if year data is missing from book, but other attributes are crucial for ranking
+        if (avg_rating == None or num_ratings == None):
+            continue
+        
+        book_dict['Avg rating'] = float(avg_rating)
+        book_dict['Number of Ratings'] = int(num_ratings)
+        book_dict['Year Published'] = int(year) if year else None
+
+        books.append(book_dict)
+
+    return books
 
 def extract_sub_text(sub_text):
 
@@ -40,9 +59,10 @@ def extract_sub_text(sub_text):
     num_ratings_match = num_rating_regex.search(sub_text)
     year_match = year_regex.search(sub_text)
     
-    avg_rating = avg_rating_match.group('avg_rating')
-    num_ratings = num_ratings_match.group('num_ratings')
+    avg_rating = avg_rating_match.group('avg_rating') if avg_rating_match else None
+    num_ratings = num_ratings_match.group('num_ratings') if num_ratings_match else None
     year = year_match.group('year') if year_match else None
+
     return avg_rating,num_ratings,year
 
 
