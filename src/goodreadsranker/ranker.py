c76e4ec685d86c5401fc88b828ae838372ae606c
Sean Ford <seanford@protonmail.com>
2024-01-22 15:00:51 -0800

    Implement score function

--- src/goodreadsranker/ranker.py
+++ src/goodreadsranker/ranker.py
@@ -1,7 +1,10 @@
+import math
 import csv
 
 def score(book_row):
-    pass
+    avg_rating = book_row['Avg rating']
+    num_ratings = book_row['Number of Ratings']
+    return 10 * (avg_rating - 3.7) + math.log10(num_ratings)
 
 def add_score_to_csv(csv_path):
     pass
