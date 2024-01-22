07a5f090af52ec8419611756a7e83e49f30972fa
Sean Ford <seanford@protonmail.com>
2024-01-22 13:47:11 -0800

    Update main.py with expected program flow

--- src/goodreadsranker/main.py
+++ src/goodreadsranker/main.py
@@ -1,8 +1,11 @@
 import scraper
+import ranker
 
 def main():
-    html = scraper.get_webpage_html()
-    scraper.parse_data(html)
+    website_url = 'https://www.goodreads.com/shelf/show/personal-development'
+    output_path = './book_data.csv'
+    scraper.scan_shelf(website_url, 25, output_path)
+    ranker.add_score_to_csv(output_path)
 
 if __name__ == '__main__':
     main()
