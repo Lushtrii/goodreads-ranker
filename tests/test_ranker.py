383b0b40508a227e18fd6996c5e1b1505ce02c14
Sean Ford <seanford@protonmail.com>
2024-01-22 13:49:06 -0800

    Implement test_score()

--- tests/test_ranker.py
+++ tests/test_ranker.py
@@ -5,7 +5,17 @@ from src.goodreadsranker import ranker
 class TestRanker(unittest.TestCase):
     
     def test_score(self):
-        pass
+        book_row_to_score = {
+        'Title': 'The 7 Habits of Highly Effective People: Powerful Lessons in Personal Change',
+        'Author': 'Stephen R. Covey',
+        'Avg rating': 4.16,
+        'Number of Ratings': 727556,
+        'Year Published': 1988
+        }
+        expected_score = 10.461866
+
+        actual_score = ranker.score(book_row_to_score)
+        self.assertAlmostEqual(expected_score, actual_score)
 
     def test_add_score_to_csv(self):
         pass
